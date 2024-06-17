355bf1cba56dc62cc60bbca039b7e22d2d959acb
Oluwafemi <oluwafemiadedeji143@gmail.com>
2024-06-17 02:08:35 +0100

    Update

--- src/features/authentication/UpdatePasswordForm.jsx
+++ src/features/authentication/UpdatePasswordForm.jsx
@@ -1,8 +1,17 @@
 import { useForm } from "react-hook-form";
+import { styled } from "styled-components";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
+// import Input from "../../ui/Input";
+
+const Input = styled.input`
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
+  padding: 0.8rem 1.2rem;
+  box-shadow: var(--shadow-sm);
+`;
 
 import { useUpdateUser } from "./useUpdateUser";
 
